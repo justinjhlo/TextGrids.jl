64b2779869c6ad326f522b44d6bf39e2ee2169b1
Justin Lo <37448886+justinjhlo@users.noreply.github.com>
2022-02-01 10:13:07 +0000

    fix separate types from readwrite

--- src/readwrite.jl
+++ src/readwrite.jl
@@ -1,22 +1,3 @@
-mutable struct Interval
-    index::Int
-    xmin::Float64
-    xmax::Float64
-    label::AbstractString
-end
-
-mutable struct Tier
-    num::Int
-    class::AbstractString
-    name::AbstractString
-    size::Int
-    xmin::Float64
-    xmax::Float64
-    contents::Vector{Interval}
-end
-
-TextGrid = Vector{Tier}
-
 # reading functions
 
 """
