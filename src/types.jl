437ecaa98a79de058839ba3fa79c9f8a8035755e
justinjhlo <justinlo2004@gmail.com>
2022-02-05 23:18:47 +0000

    make `show` react to display size

--- src/types.jl
+++ src/types.jl
@@ -33,24 +33,40 @@ function Base.show(io::IO, ::MIME"text/plain", tg::TextGrid)
     for tier in tg
         print(" ($(tier.num)) $(tier.name)")
     end
-    length(tg) > 0 && print_tiers(io, tg)
+    line_width = displaysize(io)[2] < 80 ? (displaysize(io)[2] - 5) : 75
+    length(tg) > 0 && print_tiers(io, tg, line_width)
 end
 
 function print_tiers(io::IO, tg::TextGrid, width = 75)
     println(io)
     println(io, "   +" * '-'^width * "+")
-    for tier in tg
-        print(io, rpad(tier.num, 3, ' ') * "|")
 
-        chunk_width = duration(tier) / width
-        chunk = fill(' ', width)
-        if length(tier.contents) > 1
-            in_chunk = unique([ceil(Int, (x.xmin - tier.xmin) / chunk_width) for x in tier.contents[2:end]])
-            chunk[in_chunk] .= '|'
+    if get(io, :limit, false) && length(tg) > 6
+        for tier in tg[1:3]
+            print_indiv_tier(io, tier, width)
         end
-        print(io, String(chunk))
-
-        println(io, "|")
+        println("⋮" * ' '^(3 + floor(Int, width/2)), "⋮")
+        for tier in tg[end-1:end]
+            print_indiv_tier(io, tier, width)
+        end
+    else
+        for tier in tg
+            print_indiv_tier(io, tier, width)
+        end
     end
     print(io, "   +" * '-'^width * "+")
+end
+
+function print_indiv_tier(io::IO, tier::Tier, width = 75)
+    print(io, rpad(tier.num, 3, ' ') * "|")
+    
+    chunk_width = duration(tier) / width
+    chunk = fill(' ', width)
+    if length(tier.contents) > 1
+        in_chunk = unique([ceil(Int, (x.xmin - tier.xmin) / chunk_width) for x in tier.contents[2:end]])
+        chunk[in_chunk] .= '|'
+    end
+    print(io, String(chunk))
+    
+    println(io, "|")
 end
